c718735bce986ea2f2c05c8545d754d4be12f2c3
Fred Agyeman <fredyliby@verizon.net>
2015-11-15 17:09:15 -0500

    modified images and show page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,7 +9,7 @@ class Recipe < ActiveRecord::Base
 	 reject_if: proc { |attributes|attributes['step'].blank?}, 
 	 allow_destroy: true
 	 validates :title, :description, :image, presence: true
-	has_attached_file :image, styles: { medium: "200x200#" }
+	has_attached_file :image, styles: { medium: "400x200#", large: "500x400" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
 end
